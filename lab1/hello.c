41575abc656c5937c0d5ec77fe55dd50d2279ba1
MarkusMagpie <m.sorokin3@g.nsu.ru>
2025-02-06 21:32:48 +0700

    lab1 added comments

--- lab1/hello.c
+++ lab1/hello.c
@@ -5,30 +5,31 @@ void hello_from_static_lib();
 void hello_from_dynamic_lib();
 
 // int main() {
-//     printf("Hello, world!\n");
+//     // 1
+//     // printf("Hello, world!\n");
 //     // 2
 //     // hello_from_static_lib();
 //     // 3
-//     // hello_from_dynamic_lib();
+//     hello_from_dynamic_lib();
 //     return 0;
 // }
 
 // 4
 int main() {
-    void *handle = dlopen("./libdyn_runtime.so", RTLD_LAZY); // загружает библиотеку во время выполнения
-    if (!handle) {
+    void *lib = dlopen("./libdyn_runtime.so", RTLD_LAZY); // загружаем библиотеку во время выполнения функции
+    if (!lib) {
         printf("Error loading library");
         return 0;
     }
 
-    void (*hello)() = dlsym(handle, "hello_from_dyn_runtime_lib"); // получает указатель на функцию из dyn_runtime_lib.c
+    void (*hello)() = dlsym(lib, "hello_from_dyn_runtime_lib"); // получаем указатель на функцию из dyn_runtime_lib.c
     if (!hello) {
-        printf("Error finding symbol");
+        printf("Error 2");
         return 0;
     }
 
     hello();
 
-    dlclose(handle); // освобождает библиотеку после использования
+    dlclose(lib); // освобождаем библиотеку после использования функции
     return 0;
 }
