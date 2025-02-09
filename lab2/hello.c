a013571c277bbe713be478d5a2a91ae52cf780f5
MarkusMagpie <m.sorokin3@g.nsu.ru>
2025-02-09 12:26:19 +0700

    started lab2: 1a

---
+++ lab2/hello.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+#include <unistd.h> // ii + iii
+#include <sys/syscall.h> // iii для SYS_write
+
+int main() {
+    // i
+    // printf("Hello, world!\n");
+    // ii
+    // const char msg[] = "Hello, world!\n";
+    // write(1, msg, sizeof(msg) - 1);
+    // iii
+    const char msg[] = "hello, world!\n";
+    syscall(SYS_write, 1, msg, sizeof(msg) - 1);
+
+    return 0;
+}
