0f2787ca7b1594a65a3a605bef278eb58d689096
MarkusMagpie <m.sorokin3@g.nsu.ru>
2025-02-21 16:54:19 +0700

    added lab 2 b: hello.S and some questions

--- lab1/hello.c
+++ lab1/hello.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <dlfcn.h> // 4
 
-// void hello_from_static_lib();
-// void hello_from_dynamic_lib();
+// void hello_from_static_lib(); // 2
+void hello_from_dynamic_lib(); // 3
 
-// int main() {
-//     // 1
-//     // printf("Hello, world!\n");
-//     // 2
-//     // hello_from_static_lib();
-//     // 3
-//     hello_from_dynamic_lib();
-//     return 0;
-// }
-
-// 4
 int main() {
-    void *lib = dlopen("./libdyn_runtime.so", RTLD_LAZY); // загружаем библиотеку во время выполнения функции
-    if (!lib) {
-        printf("Error 1");
-        return 0;
-    }
-
-    void (*hello)() = dlsym(lib, "hello_from_dyn_runtime_lib"); // получаем указатель на функцию из dyn_runtime_lib.c
-    if (!hello) {
-        printf("Error 2");
-        return 0;
-    }
-
-    hello();
-
-    dlclose(lib); // освобождаем библиотеку после использования функции
+// //     // 1
+// //     // printf("Hello, world!\n");
+// //     // 2
+//     // hello_from_static_lib();
+// //     // 3
+    hello_from_dynamic_lib();
     return 0;
 }
+
+// 4
+// int main() {
+//     void *lib = dlopen("./libdyn_runtime.so", RTLD_LAZY); // загружаем библиотеку во время выполнения функции
+//     if (!lib) {
+//         printf("Error 1");
+//         return 0;
+//     }
+
+//     void (*hello)() = dlsym(lib, "hello_from_dyn_runtime_lib"); // получаем указатель на функцию из dyn_runtime_lib.c
+//     if (!hello) {
+//         printf("Error 2");
+//         return 0;
+//     }
+
+//     hello();
+
+//     dlclose(lib); // освобождаем библиотеку после использования функции
+//     return 0;
+// }
