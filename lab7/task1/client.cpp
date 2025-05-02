9e7a5ae101930768b093c656b5d4ed37fab1aa0e
MarkusMagpie <m.sorokin3@g.nsu.ru>
2025-05-02 14:45:04 +0700

    7.1

---
+++ lab7/task1/client.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define BUFFER_SIZE 1024
+#define PORT 5000
+
+int main() {
+    int client_fd;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+
+    // создаем UDP сокет
+    /*
+    socket() создает сокет, возвращает дескриптор сокета
+        __domain - AF_INET - протокол IPv4
+        __type - SOCK_DGRAM - UDP
+        __protocol - 0 - выбирается автоматически
+    */
+    if ((client_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        std::cout << "socket creation failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // настройка адреса сервера server_addr
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+        std::cout << "inet_pton failed" << std::endl;
+        close(client_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "введите сообщение серверу: ";
+    std::cin.getline(buffer, BUFFER_SIZE);
+
+    /*
+        sendto() отправляет сообщение серверу через сокет: 
+            client_fd - дескриптор сокета
+            buffer - указатель на буфер с данными
+            strlen(buffer) - размер сообщения
+            0 - флаги
+            (sockaddr*)&server_addr - адрес получателя 
+            sizeof(server_addr) - его размер соответственно
+    */
+    ssize_t sent_len = sendto(client_fd, buffer, strlen(buffer), 0,
+        (sockaddr*)&server_addr, sizeof(server_addr));
+    
+    if (sent_len < 0) {
+        std::cout << "sendto failed" << std::endl;
+        close(client_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+    recfvrom() получает сообщение от сервера через сокет:
+        client_fd - дескриптор сокета
+        buffer - буфер для данными
+        BUFFER_SIZE - сколько байт считать из буффера 
+        0 - флаги
+        NULL - адрес получателя
+        NULL - размер получателя
+    */
+    ssize_t recv_len = recvfrom(client_fd, buffer, BUFFER_SIZE, 0, NULL, NULL);
+    if (recv_len < 0) {
+        std::cout << "recvfrom failed" << std::endl;
+        close(client_fd);
+        exit(EXIT_FAILURE);
+    }
+    buffer[recv_len] = '\0';
+
+    std::cout << "получено эхо-сообщение: " << buffer << std::endl;
+
+    close(client_fd);
+    return 0;
+}
---
+++ lab7/task1/server.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h> // inet_ntoa
+
+#define BUFFER_SIZE 1024
+#define PORT 5000
+
+int main() {
+    int server_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    char buffer[BUFFER_SIZE];
+
+    // создаем UDP сокет
+    /*
+    socket() создает сокет, возвращает дескриптор сокета
+        __domain - AF_INET - протокол IPv4
+        __type - SOCK_DGRAM - UDP
+        __protocol - 0 - выбирается автоматически
+    */
+    server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_fd < 0) {
+        std::cout << "socket creation failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // настройка адреса сервера server_addr
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET; // IP адрес к которому будет привязан сокет (IPv4)
+    server_addr.sin_port = htons(PORT); // номер порта (в сетевом порядке байт) к которому будет привязан сокет
+
+    // привязка сокета к адресу server_addr
+    if (bind(server_fd, (const struct sockaddr*)&server_addr, 
+            sizeof(server_addr)) < 0) {
+        std::cout << "bind failed" << std::endl;
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "сервер запущен на порту: " << PORT << std::endl;
+
+    while (true) {
+        // принимаем данные от клиента
+        /*
+        recfvrom() получает сообщение от клиента через сокет:
+            server_fd - дескриптор сокета
+            buffer - буфер для данными
+            BUFFER_SIZE - сколько байт считать из буффера 
+            0 - флаги
+            (sockaddr*)&client_addr - адрес получателя
+            &client_len - размер  получателя
+        */
+        ssize_t msg_len = recvfrom(server_fd, buffer, BUFFER_SIZE, 0,
+            (sockaddr*)&client_addr, &client_len);
+        
+        if (msg_len < 0) {
+            std::cout << "recvfrom failed" << std::endl;
+            continue;
+        }
+
+        buffer[msg_len] = '\0';
+        std::cout << "получено от " 
+                  << inet_ntoa(client_addr.sin_addr) << ":"
+                  << client_addr.sin_port << " - " 
+                  << buffer << std::endl;
+
+        // отправляем данные обратно клиенту (эхо-ответ который видим с клиентской стороны)
+        if (sendto(server_fd, buffer, msg_len, 0,
+            (const struct sockaddr*)&client_addr, client_len) < 0) {
+            std::cout << "sendto failed" << std::endl;
+        }
+    }
+
+    close(server_fd);
+    return 0;
+}
